35e020ea8b2b3bc1f0a0e96b93d6283a9129c985
Estorc <51833938+Estorc@users.noreply.github.com>
2022-05-22 20:49:05 +0200

    vgdf
    gdfgdf

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,33 @@ function parallax() {
 
 
 
+function phoneMenu() {
+	let phoneMenuList = document.getElementById('phoneMenuList');
+	if ($(phoneMenuList).css('visibility') == 'visible') {
+		
+		$(phoneMenuList).css('visibility', 'hidden')
+		$(phoneMenuList).css('opacity', '0');
+		$(phoneMenuList).css('transform', 'scale(1,0) translate(0, -100%)');
+	
+	} else {
+		
+		$(phoneMenuList).css('visibility', 'visible')
+		$(phoneMenuList).css('opacity', '1');
+		$(phoneMenuList).css('transform', 'scale(1,1) translate(0, 0)');
+		
+	}
+}
 
 
+window.addEventListener('click', function(e){   
+  if (document.getElementById('phoneMenu').contains(e.target)){
 
+  } else {
+    $(phoneMenuList).css('visibility', 'hidden')
+	$(phoneMenuList).css('opacity', '0');
+	$(phoneMenuList).css('transform', 'scale(1,0) translate(0, -100%)');
+  }
+});
 
 
 
